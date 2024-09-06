57ba9355c06ce7765b7313293e241a01af0c8b64
michaelong7 <michael.ong@mail.utoronto.ca>
2024-09-06 12:49:58 -0400

    removed dependency on pydrobert torch

--- get_single_k.py
+++ get_single_k.py
@@ -15,12 +15,154 @@
 # limitations under the License.
 
 import sys
-import argparse
+from typing import (
+    List,
+    Optional,
+    TextIO,
+    Tuple,
+    Union,
+)
+from multiprocessing import Pool
 
 import numpy as np
 import jiwer
 
-from pydrobert.torch.data import read_trn_iter
+# _AktTree is modified from the pydrobert-pytorch package
+class _AltTree(object):
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.tokens = []
+        if parent is not None:
+            parent.tokens.append([self.tokens])
+
+    def new_branch(self):
+        assert self.parent
+        self.tokens = []
+        self.parent.tokens[-1].append(self.tokens)
+
+# _trn_line_to_transcript is modified from the pydrobert-pytorch package
+def _trn_line_to_transcript(x: Tuple[str, bool]) -> Optional[Tuple[str, List[str]]]:
+    line, warn = x
+    line = line.strip()
+    if not line:
+        return None
+    try:
+        last_open = line.rindex("(")
+        last_close = line.rindex(")")
+        if last_open > last_close:
+            raise ValueError()
+    except ValueError:
+        raise IOError("Line does not end in utterance id")
+    utt_id = line[last_open + 1 : last_close]
+    line = line[:last_open].strip()
+    transcript = []
+    token = ""
+    alt_tree = _AltTree()
+    found_alt = False
+    while len(line):
+        c = line[0]
+        line = line[1:]
+        if c == "{":
+            found_alt = True
+            if token:
+                if alt_tree.parent is None:
+                    transcript.append(token)
+                else:
+                    alt_tree.tokens.append(token)
+                token = ""
+            alt_tree = _AltTree(alt_tree)
+        elif c == "/" and alt_tree.parent is not None:
+            if token:
+                alt_tree.tokens.append(token)
+                token = ""
+            alt_tree.new_branch()
+        elif c == "}" and alt_tree.parent is not None:
+            if token:
+                alt_tree.tokens.append(token)
+                token = ""
+            if not alt_tree.tokens:
+                raise IOError('Empty alternate found ("{ }")')
+            alt_tree = alt_tree.parent
+            if alt_tree.parent is None:
+                assert len(alt_tree.tokens) == 1
+                transcript.append((alt_tree.tokens[0], -1, -1))
+                alt_tree.tokens = []
+        elif c == " ":
+            if token:
+                if alt_tree.parent is None:
+                    transcript.append(token)
+                else:
+                    alt_tree.tokens.append(token)
+                token = ""
+        else:
+            token += c
+    if token and alt_tree.parent is None:
+        transcript.append(token)
+    if found_alt and warn:
+        warnings.warn(
+            'Found an alternate in transcription for utt="{}". '
+            "Transcript will contain an array of alternates at that "
+            "point, and will not be compatible with transcript_to_token "
+            "until resolved. To suppress this warning, set warn=False"
+            "".format(utt_id)
+        )
+    return utt_id, transcript
+
+# read_trn_iter is modified from the pydrobert-pytorch package
+def read_trn_iter(
+    trn: Union[TextIO, str],
+    warn: bool = True,
+    processes: int = 0,
+    chunk_size: int = 1000,
+) -> Tuple[str, List[str]]:
+    """Read a NIST sclite transcript file, yielding individual transcripts
+
+    Identical to :func:`read_trn`, but yields individual transcript entries rather than
+    a full list. Ideal for large transcript files.
+
+    Parameters
+    ----------
+    trn
+    warn
+    processes
+    chunk_size
+
+    Yields
+    ------
+    utt_id : str
+    transcript : list of str
+    """
+    # implementation note: there's a lot of weirdness here. I'm trying to
+    # match sclite's behaviour. A few things
+    # - the last parentheses are always the utterance. Everything else is
+    #   the word
+    # - An unmatched '}' is treated as a word
+    # - A '/' not within curly braces is a word
+    # - If the utterance ends without closing its alternate, the alternate is
+    #   discarded
+    # - Comments from other formats are not comments here...
+    # - ...but everything passed the last pair of parentheses is ignored...
+    # - ...and internal parentheses are treated as words
+    # - Spaces are treated as part of the utterance id
+    # - Seg faults on empty alternates
+    if isinstance(trn, str):
+        with open(trn) as trn:
+            yield from read_trn_iter(trn, warn, processes)
+    elif processes == 0:
+        for line in trn:
+            x = _trn_line_to_transcript((line, warn))
+            if x is not None:
+                yield x
+    else:
+        with Pool(processes) as pool:
+            transcripts = pool.imap(
+                _trn_line_to_transcript, ((line, warn) for line in trn), chunk_size
+            )
+            for x in transcripts:
+                if x is not None:
+                    yield x
+            pool.close()
+            pool.join()
 
 def get_ln_err(ref_file, hyp_file):
     ref_dict = dict(read_trn_iter(ref_file))
